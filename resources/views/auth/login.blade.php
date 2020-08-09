b81c694766e76c1e474426a43aa1c8e59fc26ea0
Daniel Ng`andu <daniel_ngandu@outlook.com>
2020-08-09 15:58:41 +0200

    More adds

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,7 +53,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary"><i class="fas fa-arrow-circle-right"></i>
                                     {{ __('Login') }}
                                 </button>
 
